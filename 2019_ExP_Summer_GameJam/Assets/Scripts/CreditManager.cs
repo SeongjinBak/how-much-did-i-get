e3e559d26c6c52c5d4ebe8c20469ced45a5fbef7
seongjinbak <seongjinbak.sjb@gmail.com>
2019-11-09 21:09:06 +0900

    Credit Manager class 코드 정리

--- 2019_ExP_Summer_GameJam/Assets/Scripts/CreditManager.cs
+++ 2019_ExP_Summer_GameJam/Assets/Scripts/CreditManager.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
+﻿/*
+ *  크레딧 씬에서 화면이 터치될 때 까지 크레딧 화면을 보여주는 스크립트 입니다.
+ */ 
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CreditManager : MonoBehaviour
 {
-    
-
     // Update is called once per frame
     void Update()
     {
