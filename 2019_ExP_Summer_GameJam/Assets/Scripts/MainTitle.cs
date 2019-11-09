c25c8e3cb302b9a2f34f5d0ceac2534ff5c3e1b5
seongjinbak <seongjinbak.sjb@gmail.com>
2019-11-09 21:09:37 +0900

    Main title class 코드 정리

--- 2019_ExP_Summer_GameJam/Assets/Scripts/MainTitle.cs
+++ 2019_ExP_Summer_GameJam/Assets/Scripts/MainTitle.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿/*
+ * 게임 처음 시작시 나오는 메인 화면 스크립트 입니다.
+ * 아무 입력을 받게 되면, 스토리 씬으로 넘어가게 됩니다.
+ */ 
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainTitle : MonoBehaviour
 {
-
-
     // Update is called once per frame
     void Update()
     {
